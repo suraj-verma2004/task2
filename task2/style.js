181e45cfca9de4bf7cf330d3184848a25ab17c17
suraj-verma2004 <147896230+suraj-verma2004@users.noreply.github.com>
2025-05-27 15:11:49 +0530

    Add files via upload

---
+++ task2/style.js
@@ -0,0 +1,39 @@
+function addTask() {
+  const taskInput = document.getElementById("taskInput");
+  const taskText = taskInput.value.trim();
+
+  if (taskText === "") {
+    alert("Please enter a task.");
+    return;
+  }
+  // Constructiing a list 
+  const li = document.createElement("li");
+  li.textContent = taskText;
+
+  // Adding delter butoon 
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "X";
+  deleteBtn.className = "delete-btn";
+  deleteBtn.onclick = function () {
+    li.remove();
+  };
+
+  //  adding a check button 
+ const cmp = document.createElement("button");
+  cmp.textContent = "✔";
+  cmp.className = "cmp";  
+  cmp.style.marginLeft="10px";
+  cmp.onclick = function () {
+    li.style.backgroundColor="green";
+    cmp.remove();  
+  };
+
+
+  // appending the buttons into list 
+
+  li.appendChild(cmp)
+  li.appendChild(deleteBtn);
+  document.getElementById("taskList").appendChild(li);
+ 
+  taskInput.value = "";
+}
